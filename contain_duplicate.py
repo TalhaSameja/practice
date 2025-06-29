7ad993e16482c932f7c93cd5e644555111860efb
Talha Yousuf Sameja <talha.samej@gmail.com>
2025-06-29 17:06:53 +0500

    solved the contain duplicate problem

---
+++ contain_duplicate.py
@@ -0,0 +1,7 @@
+
+def containsDuplicate(nums)
+    nums.sort()
+    for i in range(1,len(nums)):
+        if nums[i] == nums[i-1]:
+            return True
+    return False
--- practice.py
+++ practice.py
@@ -1,9 +1,14 @@
-n = [2,4,5,7]
-target = 11
-for i in range(len(n)):
-    for j in range(i+1, len(n)):
-        if n[i]+n[j] == target:
-            print(i,j)
+n = [2,4,5,2]
+
+n.sort()
+#[2,2,4,5]
+
+# for i in range(len(n)):
+#     for j in range(i+1, len(n)):
+#         if n[i] == n[j]:
+#             print(i,j)
             
-            
+for i in range(1,len(n)):
+    if n[i] == n[i-1]:
+        print(True)        
             
